20d4d35793313c0f646fdac2e78cf9a95b089459
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-13 23:32:29 -0400

    started work on index.js logic

--- Main/db/index.js
+++ Main/db/index.js
@@ -1,1 +1,15 @@
-intial commit
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+require("console.table");
+
+init();
+
+// Display logo text, load main prompts
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+
+  console.log(logoText);
+
+  loadMainPrompts();
+}
