7aca2ebc2597aeebd5ba42a0ddb85e1614248fcb
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-13 22:32:32 -0400

    made changes to .sql DB template

--- Main/db/connection.js
+++ Main/db/connection.js
@@ -1,1 +1,17 @@
-intial commit
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  user: "root",
+  // Your password
+  password: "",
+  database: "employees"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
--- employeeTracker.sql
+++ employeeTracker.sql
@@ -21,8 +21,21 @@ CREATE TABLE role (
 )
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
-
 department_id INT NOT NULL,
 FOREIGN KEY (department_id) REFERENCES department (id),
 PRIMARY KEY (id)
+);
 
+CREATE TABLE employee (
+
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INT NOT NULL, 
+  FOREIGN KEY (manager_id) REFERENCES role(id),
+  PRIMARY KEY (id)
+
+
+);
