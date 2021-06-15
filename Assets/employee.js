0a8b70411c0f6352cb878e4efce6e92cb03a1770
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-14 21:59:18 -0400

    fixed tyop on line 6 in employee.js

--- Assets/employee.js
+++ Assets/employee.js
@@ -3,7 +3,7 @@ class Employee {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
-        this.role_id, manager_id;
+        this.role_id = manager_id;
         this.manager_id = manager_id;
     }
 }
