8256f443e936dd8a2894dadf624dc788bcda5449
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-14 22:27:22 -0400

    working on setting up importing classes and npm require

---
+++ Main/index.js
@@ -0,0 +1,7 @@
+const Department = require('./assets/department_id')
+const Role = require('./assets/role_id');
+const Employee = require('./assets/employee');
+
+const inquirer = require('inquirer')
+const cTable = require('console.table');
+require('dotenv'.config();
