c3511194bca1eaf6f5db578b4e24eb3ec9c467bb
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-14 21:45:08 -0400

    working on department.js logic

---
+++ Assets/department.js
@@ -0,0 +1,16 @@
+class Department (
+    constructor(id, name) {
+        this.id;
+        this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+    getName() {
+        return this.name;
+    }
+}
+
+module.export = Department;
+    
