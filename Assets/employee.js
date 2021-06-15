1a560124dc45b7c0bb8086c03cce1fbf41614e21
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-14 21:58:40 -0400

    working on return this for constructors

--- Assets/employee.js
+++ Assets/employee.js
@@ -2,6 +2,8 @@ class Employee {
     constructor(id, first_name, last_name, role_id, manager_id) {
         this.id = id;
         this.first_name = first_name;
-        this.last_name
+        this.last_name = last_name;
+        this.role_id, manager_id;
+        this.manager_id = manager_id;
     }
 }
