4e93e2b0c74a67ed4ca1f0877d9a8905b982adc9
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-14 21:34:24 -0400

    working on index.js code

--- Main/db/index.js
+++ Main/db/index.js
@@ -13,3 +13,19 @@ function init() {
 
   loadMainPrompts();
 }
+
+async function loadMainPrompts() {
+  const { choice } = await prompt([
+{
+  type: "list",
+  name: "choice",
+  message: "What would you like to do",
+  choices: [
+     {
+        name: "View All Employees",
+        value: "VIEW_EMPLOYEES"
+      },
+      {
+    
+  }
+}
