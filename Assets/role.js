249e1a30303645e3c131b951f5616ed0d2eaa8d5
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-20 15:51:38 -0400

    fixed typo in role.js for id

--- Assets/role.js
+++ Assets/role.js
@@ -1,6 +1,6 @@
 class Role {
     constructor(id, title, salary, department_id) {
-        this.id;
+        this.id = id;
         this.title = title;
         this.salary = salary;
         this.department_id = department_id
